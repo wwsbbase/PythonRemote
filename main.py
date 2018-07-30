7d5bb246fd423807fbad5ddafca7a401ec5cb661
D.J. Zou <bopy2008@163.com>
2018-07-30 23:27:50 +0800

    connect mysql , upload the temper

--- main.py
+++ main.py
@@ -4,8 +4,26 @@
 import ptvsd
 ptvsd.settrace(None, ('0.0.0.0', 18110))
 
+
 from time import sleep
 from random import random
+
+import pymysql
+conn = pymysql.connect(host='111.231.228.46', user='bopy', passwd='yousendit', db='tempDB')
+cur = conn.cursor()
+
+
+def init():
+    """init"""
+    cur.execute("use tempDB")
+
+def uninit():
+    cur.close()
+    conn.close()
+
+def store(temp):
+    cur.execute("INSERT INTO tempRecord (temp) VALUES (%s)",(temp))
+    conn.commit()
 
 def get_cpu_temp():
     cpu_temp_file = open( "/sys/class/thermal/thermal_zone0/temp" )
@@ -15,19 +33,25 @@ def get_cpu_temp():
 
 def measure_temp():
     """vcgencmd measure_temp"""
-    print(get_cpu_temp())
+    temp = get_cpu_temp()
+    # store(get_cpu_temp())
+    store(int(temp))
+    print(temp)
 
 
 def main():
     """docstring for main"""
     print("main start")
-    ptvsd.wait_for_attach()
+    # ptvsd.wait_for_attach()
+    init()
+
     for _ in range(0, 50):
         sleep(5)
-        i = random()
-        print(i)
+        # i = random()
+        # print(i)
         measure_temp()
 
+    uninit()
     print("main done")
     
 if __name__ == '__main__':
